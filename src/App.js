2f6384c9b75163f9fb6af52f7c97a606ac391006
Utkarsh Mishra <utkarshmishra2022@u.northwestern.edu>
2020-03-07 06:11:47 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import SplashPage from './Pages/SplashPage';
 import WorkoutsPage from './Pages/WorkoutsPage';
 import TopMenu from './Components/TopMenu';
-import ProfilePage from './Pages/ProfilePage';
-import { WorkoutContext } from './context';
+import ProfilePage from './Pages/ProfilePage'import { WorkoutContext } from './context';
 import { firebase }from './firebaseDb';
 import 'firebase/database';
 import "firebase/auth";
