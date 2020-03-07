34724472be820032b98a8ce7dd19740d3cfd382c
Utkarsh Mishra <utkarshmishra2022@u.northwestern.edu>
2020-03-07 04:03:36 -0600

    travis

---
+++ build/precache-manifest.a2f92924a0fc7bca6651ec69d4d15db8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d68037182b858c1a6073885607d7fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cb223d72675cc7a6f23",
+    "url": "/static/js/2.787ea28a.chunk.js"
+  },
+  {
+    "revision": "f06bfedc54356bc13807403f55b38f97",
+    "url": "/static/js/2.787ea28a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bfd8f11443d973573b0",
+    "url": "/static/js/main.b9936815.chunk.js"
+  },
+  {
+    "revision": "45ccb2a213c3e1951939",
+    "url": "/static/js/runtime-main.920456b5.js"
+  }
+]);
